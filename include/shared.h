333e9acd2fbe736653df3587f4220676d3cb4504
Bryan Bugyi <bryanbugyi34@gmail.com>
2018-06-27 08:02:44 -0400

    Meta: update submodule gutils

--- include/shared.h
+++ include/shared.h
@@ -1,9 +1,9 @@
-#ifndef INCLUDED_COMMON
-#define INCLUDED_COMMON
+#ifndef INCLUDED_SHARED
+#define INCLUDED_SHARED
 
 #include <gutils.h>
 
 const std::string PROJECT_NAME = "countdown";
 const std::string XDG_RUNTIME_DIR = gutils::init_xdg_dir(PROJECT_NAME, "runtime");
 
-#endif /* INCLUDED_COMMON */
+#endif /* INCLUDED_SHARED */
--- src/process.cc
+++ src/process.cc
@@ -4,7 +4,6 @@
 #include <unistd.h>
 #include <signal.h>
 
-#include <gutils.h>
 #include <process.h>
 #include <shared.h>
 
